d1c0737bff2d55ab592d4bd783e9a669d3edb4a2
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-05-17 17:50:44 +0530

    NFT marketplace

---
+++ Practice tokens/Assignment7.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CrowdFunding{
+
+    struct projectDetails{
+        string name;
+        string description;
+        uint funding_goal;
+        uint deadLine;
+    }
+
+}
