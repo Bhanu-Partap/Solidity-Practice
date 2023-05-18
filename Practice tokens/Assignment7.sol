b46f315ba8a328f977646c563f2560c6a83364ee
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-05-18 10:27:31 +0530

    crowdfunding

--- Practice tokens/Assignment7.sol
+++ Practice tokens/Assignment7.sol
@@ -1,26 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CrowdFunding{
-
-    struct projectDetails{
+contract CrowdFunding {
+    struct projectDetails {
         string name;
         string description;
-        uint funding_goal;
-        uint deadLine;
+        uint256 funding_goal;
+        uint256 deadline;
     }
 
-    uint id;
+    uint256 id;
     projectDetails[] public project;
 
     // mapping
-    mapping(uint => projectDetails) public userProjects;
+    mapping(uint256 => projectDetails) public userProjects;
 
-
-    function setProjectDetails() public {
-
+    constructor(uint256 _funding_goal, uint256 _deadline) {
+        // owner = msg.sender;
+        userProjects[id].funding_goal = _funding_goal;
+        userProjects[id].deadline = block.timestamp + _deadline;
     }
 
+    function setProjectDetails( uint256 _id, string memory _name,string memory _description ) public {
+        userProjects[_id].name = _name;
+        userProjects[_id].description = _description;
+        userProjects[_id].name = _name;
+        userProjects[_id].name = _name;
+        userProjects[_id].name = _name;
 
 
+    }
 }
