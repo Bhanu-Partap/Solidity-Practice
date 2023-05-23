00685c262a7eac4fa6601b9054c34ca8a1a0e2c1
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-05-23 11:02:13 +0530

    crowdfunding

--- Assignment 4 Tokens/ERC-1155.sol
+++ Assignment 4 Tokens/ERC-1155.sol
@@ -1,37 +1,48 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+// OpenZeppelin Contracts (last updated v4.8.0) (token/ERC1155/ERC1155.sol)
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+pragma solidity ^0.8.0;
 
-contract MyToken is ERC1155, Ownable, ERC1155Supply {
-    constructor() ERC1155("") {}
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/extensions/IERC1155MetadataURI.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol";
+
+contract ERC1155MYTOKEN  is Ownable, ERC1155Supply {
+        uint256 public maxSupply= 20;
+         uint256 public PublicPrice= 1 wei;
+     //mapping(uint256 => address) public _owners;
+   uint public constant Token_Id=1;
+      mapping(uint256 => uint256) public _totalSupply;
+
+    constructor()
+        ERC1155("https://gateway.pinata.cloud/ipfs/QmW6pWuXAneY12Tb1w272A1GpckkXeULCH6zkM9Pk4mi9W/metadata-NFT1")
+    {
+             _mint(msg.sender, Token_Id, 100, "");
+          //   _totalSupply[Token_Id] = 100;
+    }
+
+    
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
-
-    function mint(address account, uint256 id, uint256 amount, bytes memory data)
-        public
-        onlyOwner
-    {
-        _mint(account, id, amount, data);
+      function uri(uint256 _id) public view virtual override returns (string memory) {
+      require(exists(_id),"uri not exists token");
+      return string(abi.encodePacked(super.uri(_id), Strings.toString(_id),".json"));
     }
+     function ownerOf(uint256 _id, address owner)public  view  returns(bool){
+         return balanceOf(owner, _id)!=0;
+     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
-    {
-        _mintBatch(to, ids, amounts, data);
-    }
+        function mint(uint256 id, uint256 amount) public payable {
+        require (msg.value>=PublicPrice*_amount,"not enough money");
+         require(_totalSupply[_id]+ _amount <= maxSupply, "Exceeds maximum supply");
+        mint(msg.sender,_id, amount, "");
+         totalSupply[_id] += amount;
 
-    // The following functions are overrides required by Solidity.
+    }  
 
-    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        internal
-        override(ERC1155, ERC1155Supply)
-    {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-    }
-}
+ }
