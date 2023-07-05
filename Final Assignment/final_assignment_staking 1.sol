d767611ab39e22a007a49eca53617649f2f24c58
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-05 14:00:39 +0530

    staking

--- Final Assignment/final_assignment_staking 1.sol
+++ Final Assignment/final_assignment_staking 1.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+import "./ERC-20.sol";
+
+
 contract Staking {
 
+    ERC20Basic Token;
 
+     constructor(ERC20Basic _addressERC20 ){
+        Token=_addressERC20;
+    }
 
 }
