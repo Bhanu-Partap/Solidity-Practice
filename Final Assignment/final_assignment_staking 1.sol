d69d34765c8cea233d47d4b374387754725f8a5e
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-05 14:04:47 +0530

    staking

--- Final Assignment/final_assignment_staking 1.sol
+++ Final Assignment/final_assignment_staking 1.sol
@@ -4,12 +4,19 @@ pragma solidity ^0.8.10;
 import "./ERC-20.sol";
 
 
-contract Staking {
+contract Staking_Token {
 
     ERC20Basic Token;
 
      constructor(ERC20Basic _addressERC20 ){
         Token=_addressERC20;
+    }
+
+    struct Stake{
+        uint stake_amount;
+        uint stake_reward;
+        string stake_type;
+        uint stake_time;
     }
 
 }
