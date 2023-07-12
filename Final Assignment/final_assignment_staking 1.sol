d1d10b8f9b3d6fd259e4b5d7ecf39fd360e04835
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-12 11:29:30 +0530

    staking

--- Final Assignment/final_assignment_staking 1.sol
+++ Final Assignment/final_assignment_staking 1.sol
@@ -29,44 +29,44 @@ contract Staking_Token {
     uint256 totalIntrestAmount;
     uint256 finalAmount;
 
-    mapping(address => Stake) public Stake_details;
+    mapping(address => Stake[]) public Stake_details;
     // mapping(address => uint256) public balances;
 
     event tokensStaked(address from, uint256 amount, uint256 _duration);
 
 
-    function staking(uint256 _amount,string memory _type,uint256 _duration, bool _isFixed) public {
+    function staking(uint256 _amount,string memory _type,uint256 _duration, bool _isFixed, uint _index) public {
         require(Token.balanceOf(msg.sender) >= _amount, "Insufficient Balance");
         if ( keccak256(abi.encodePacked(_type)) == keccak256(abi.encodePacked("fixed")) ) {
             require( _amount > 0," Stake can not be 0 , Enter some amount of tokens");
-            Stake_details[msg.sender].stake_amount = _amount;
-            Stake_details[msg.sender].stake_type = _type;
-            Stake_details[msg.sender].stake_time = block.timestamp + _duration;
-            Stake_details[msg.sender].isFixed = _isFixed;
-            Stake_details[msg.sender].starting_stake_time = block.timestamp;
+            Stake_details[msg.sender][_index].stake_amount = _amount;
+            Stake_details[msg.sender][_index].stake_type = _type;
+            Stake_details[msg.sender][_index].stake_time = block.timestamp + _duration;
+            Stake_details[msg.sender][_index].isFixed = _isFixed;
+            Stake_details[msg.sender][_index].starting_stake_time = block.timestamp;
             Token.transferFrom(msg.sender, address(this), _amount);
             emit tokensStaked(msg.sender, _amount, block.timestamp);
         } 
         else if (keccak256(abi.encodePacked(_type)) == keccak256(abi.encodePacked("unfixed")) ) {
-            Stake_details[msg.sender].stake_amount = _amount;
-            Stake_details[msg.sender].stake_type = _type;
-            Stake_details[msg.sender].isFixed = _isFixed;
+            Stake_details[msg.sender][_index].stake_amount = _amount;
+            Stake_details[msg.sender][_index].stake_type = _type;
+            Stake_details[msg.sender][_index].isFixed = _isFixed;
             Token.transferFrom(msg.sender, address(this), _amount);
-            Stake_details[msg.sender].starting_stake_time = block.timestamp;
+            Stake_details[msg.sender][_index].starting_stake_time = block.timestamp;
             Token.transferFrom(msg.sender, address(this), _amount);
             emit tokensStaked(msg.sender, _amount, block.timestamp);
         }
     }
 
-    function unstaking(address _address) public returns (uint256) {
+    function unstaking(address _address,uint256 _index) public returns (uint256) {
         console.log("hello");
 
-        if (Stake_details[_address].isFixed == true) {
+        if (Stake_details[_address][_index].isFixed == true) {
             // require(Stake_details[_address].stake_time > expirytime_forfixedstaking );
             if (block.timestamp > expirytime_forfixedstaking ) {
                 console.log("inside the fixed stake after complete time");
-                Interest =(Stake_details[_address].stake_amount *fixedinterest_rate *(Stake_details[_address].stake_time)) /(100 * 365 days);
-                totalIntrestAmount =(Stake_details[_address].stake_amount + Interest)/365 days;
+                Interest =(Stake_details[_address][_index].stake_amount *fixedinterest_rate *(Stake_details[_address][_index].stake_time)) /100;
+                totalIntrestAmount =(Stake_details[_address][_index].stake_amount + Interest)/365 days;
                 console.log(totalIntrestAmount);
                 Token.transfer(_address, totalIntrestAmount);
                 return totalIntrestAmount;
@@ -75,22 +75,23 @@ contract Staking_Token {
             //unstaked before fixed time so the penality will be taken
             else if (block.timestamp < expirytime_forfixedstaking) {
                 console.log("inside the fixed stake before complete time and got penality");
-                require(  Stake_details[_address].stake_time <  expirytime_forfixedstaking,"" );
-                Interest = (Stake_details[_address].stake_amount * fixedinterest_rate *(block.timestamp -Stake_details[_address].starting_stake_time))/ (100 * 365 days) ;
+                require(  Stake_details[_address][_index].stake_time <  expirytime_forfixedstaking,"" );
+                Interest = (Stake_details[_address][_index].stake_amount * fixedinterest_rate *(block.timestamp -Stake_details[_address][_index].starting_stake_time))/ 100 ;
                 console.log(Interest);
                 totalIntrestAmount = (Interest * 96) / 100;
                 console.log(totalIntrestAmount);
-                finalAmount =totalIntrestAmount +Stake_details[_address].stake_amount;
+                finalAmount =totalIntrestAmount +Stake_details[_address][_index].stake_amount;
                 console.log(finalAmount);
                 Token.transfer(_address, finalAmount);
                 return finalAmount;
             }
         } 
-        else if (Stake_details[_address].isFixed == false) {
+        else if (Stake_details[_address][_index].isFixed == false) {
             console.log("not fixed loop");
-            Interest =(Stake_details[_address].stake_amount *unfixedinterest_rate *( block.timestamp - Stake_details[_address].starting_stake_time)) /(100 *365 days) ;
+            // uint256 unfixed_time_cal=block.timestamp - Stake_details[_address].starting_stake_time;
+            Interest =(Stake_details[_address][_index].stake_amount *unfixedinterest_rate) /100 ;
                 console.log(Interest);
-                totalIntrestAmount =Stake_details[_address].stake_amount + Interest;
+                totalIntrestAmount =Stake_details[_address][_index].stake_amount + Interest;
                 console.log(totalIntrestAmount);
                 Token.transfer(_address, totalIntrestAmount);
                 return totalIntrestAmount;
